88b09ab7b2cb39aab83be9d00d6dfba6e385ebd8
simmsju <simmsju@ssfs.org>
2021-09-08 09:55:48 -0400

    initial Karel

--- src/MountainClimb.java
+++ src/MountainClimb.java
@@ -6,14 +6,47 @@ public class MountainClimb implements Directions {
   {
     World.readWorld("worlds/fig2-9.kwld");
     World.setVisible(true);
-    World.setDelay(75);
+    World.setDelay(50);
     //World.showSpeedControl(true);
     
     int row = 1; 
     int col = 2;
     UrRobot bot = new UrRobot(row, col,
                                 North, 1);
-
+    for (int x = 0; x <= 2; x++){
+      bot.turnLeft();
+    }
+    bot.move();
+    bot.turnLeft();
+    bot.move();
+    bot.move();
+    for (int x = 0; x <= 2; x++){
+      bot.turnLeft();
+    }
+    bot.move();
+    bot.turnLeft();
+    for (int x = 0; x <= 1; x++) {
+      bot.move();
+    }
+    for (int x = 0; x <= 2; x++){
+      bot.turnLeft();
+    }
+    bot.move();
+    bot.putBeeper();
+    bot.move();
+    for (int x = 0; x <= 2; x++) {
+      bot.turnLeft();
+    }
+    for (int x = 0; x <= 1; x++) {
+    bot.move();
+    }
+    bot.turnLeft();
+    bot.move();
+    for (int x = 0; x <= 2; x++) {
+      bot.turnLeft();
+    }
+    bot.move();
+    bot.move();
   }
   
 }
--- src/ShoppingBag.java
+++ src/ShoppingBag.java
@@ -6,13 +6,47 @@ public class ShoppingBag implements Directions {
     {
         World.readWorld("worlds/fig2-10.kwld");
         World.setVisible(true);
-        World.setDelay(75);
+        World.setDelay(10);
         //World.showSpeedControl(true);
 
         int row = 5;
         int col = 7;
         UrRobot bot = new UrRobot(row, col,
                 East, 0);
-
+        for (int x = 0; x <= 1; x++) {
+            bot.turnLeft();
+        }
+        bot.move();
+        bot.move();
+        for (int x = 0; x <= 2; x++) {
+            bot.turnLeft();
+        }
+        bot.pickBeeper();
+        bot.move();
+        bot.turnLeft();
+        bot.move();
+        bot.pickBeeper();
+        for (int x = 0; x <= 2; x++) {
+            bot.turnLeft();
+        }
+        bot.move();
+        bot.turnLeft();
+        bot.move();
+        bot.pickBeeper();
+        for (int x = 0; x <= 2; x++) {
+            bot.turnLeft();
+        }
+        bot.move();
+        bot.pickBeeper();
+        for (int x = 0; x <= 1; x++) {
+            bot.turnLeft();
+        }
+        for (int x = 0; x <= 2; x++) {
+            bot.move();
+        }
+        bot.turnLeft();
+        for (int x = 0; x <= 3; x++) {
+            bot.move();
+        }
     }
 }
